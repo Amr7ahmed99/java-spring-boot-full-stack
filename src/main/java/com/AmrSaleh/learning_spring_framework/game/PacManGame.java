4db057b8cb6bb8d6cc4a1d673528475dc1f02e10
Amr-Saleh-Omar <amr.omar@maxab.io>
2025-04-15 14:19:15 +0200

    Setup loosely coupled java gaming app

---
+++ src/main/java/com/AmrSaleh/learning_spring_framework/game/PacManGame.java
@@ -0,0 +1,19 @@
+package com.AmrSaleh.learning_spring_framework.game;
+
+public class PacManGame implements GameConsole{
+	public PacManGame(){}
+	
+	public void up() {
+		System.out.println("Up");
+	}
+	public void down() {
+		System.out.println("Down");
+	}
+	public void left() {
+		System.out.println("Left");
+	}
+	public void right() {
+		System.out.println("Right");
+	}
+}
+
